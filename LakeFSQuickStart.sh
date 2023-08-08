15e7767978541c47b8e8da9ae88d01fcbacf809e
Vladimir <vgl@ad.unc.edu>
2023-08-08 18:01:03 -0400

    Added LakeFS docker-compose.test.yaml Added LakeFSQuickStart.sh

---
+++ LakeFSQuickStart.sh
@@ -0,0 +1,6 @@
+docker run --name lakefs --pull always \
+             --rm --publish 8000:8000 \
+             treeverse/lakefs:latest \
+             run --quickstart
+
+#docker exec -it lakefs lakectl config
