a1b7822271eddf944f0bbda7f4a0d29acc4663f1
Vladimir G <vglazunov@renci.org>
2023-11-09 14:53:20 -0500

    Update to support Lakefs 1.1

--- tst/operations/LakeFsWrapperTests.py
+++ tst/operations/LakeFsWrapperTests.py
@@ -23,11 +23,11 @@ class LakeFsWrapperTests(unittest.TestCase):
         create_dirs(["temp"])
 
     def get_config(self) -> Configuration:
-        config = lakefs_client.Configuration(host='http://localhost:8001/api/v1',
+        config = lakefs_sdk.Configuration(host='http://localhost:8001/api/v1',
                                              username='AKIAIOSFOLQUICKSTART',
                                              password='wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY')
 
-        # config = lakefs_client.Configuration(host='http://lakefs2.apps.renci.org:8000/api/v1',
+        # config = lakefs_sdk.Configuration(host='http://lakefs2.apps.renci.org:8000/api/v1',
         #                                      username='<ENTERHERE>',
         #                                      password='<ENTERHERE>')
 
