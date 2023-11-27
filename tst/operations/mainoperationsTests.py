846f155a8ca58b38c4e1199a39153f581f584ad7
Vladimir G <vglazunov@renci.org>
2023-11-27 16:15:00 -0500

    Added test for get_last_input_commit_id

--- tst/operations/mainoperationsTests.py
+++ tst/operations/mainoperationsTests.py
@@ -3,7 +3,7 @@ import shutil
 
 from lakefs_sdk import Configuration
 
-from avalon.mainoperations import put_files, get_files, get_commit_id_by_input_commit_id
+from avalon.mainoperations import put_files, get_files, get_commit_id_by_input_commit_id, get_last_input_commit_id
 from avalon.operations.LakeFsWrapper import LakeFsWrapper
 from avalon.operations.files import get_filepaths, create_dirs
 
@@ -123,10 +123,18 @@ class MainOperationsTests(unittest.TestCase):
                   changes_from=commit_id)
 
         result = get_filepaths(LOCALTEMPPATH)
-        self.assertListEqual(result, ['temp/result/.empty',
+        self.assertListEqual(result, ['temp/.empty',
                                       'temp/result/file4.txt',
                                       'temp/result/dir1/file3.txt',
                                       'temp/result/dir1/file2.txt'])
+
+    def test_get_last_input_commit_id(self):
+        #need to run after test_put_files_update_files
+        lfs = LakeFsWrapper(configuration=self.get_config())
+        input_commit_id = get_last_input_commit_id(branch="main", lake_fs_client=lfs, remote_path="result", repo=REPO)
+        self.assertEquals("1" * 63 + "3", input_commit_id)
+
+
 
     def remove_tempresult_folder(self):
         shutil.rmtree(LOCALTEMPPATH)
